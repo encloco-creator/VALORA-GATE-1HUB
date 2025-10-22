4b489aeb5494ede10a2341b0825d51d4157742b3
encloco-creator <enclo.co@gmail.com>
2025-10-22 18:24:59 +0000

    fix: utf-8 encoding, layout base com header/footer, hero+CTA, login sem shadcn, textos profissionais

--- src/config/settings.js
+++ src/config/settings.js
@@ -4,5 +4,5 @@ export default {
 	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	showPlug: true, // Disable this if you want to remove the plug from the footer. (â¯Â°â¡Â°)â¯ï¸µ â»ââ»
 };
